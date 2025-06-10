b33a4bdd54eca2ff1af7821e27fef8cb936a5cd0
mike02019 <michaelanang1905@gmail.com>
2025-06-10 19:23:48 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.endOfWord = false;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word) {
+    let node = this.root;
+    for (let char of word) {
+      if (!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+      node = node.children[char];
+    }
+    node.endOfWord = true;
+  }
+
+  search(word) {
+    let node = this._searchPrefix(word);
+    return node !== null && node.endOfWord === true;
+  }
+
+  startsWith(prefix) {
+    return this._searchPrefix(prefix) !== null;
+  }
+
+  _searchPrefix(prefix) {
+    let node = this.root;
+    for (let char of prefix) {
+      if (!node.children[char]) {
+        return null;
+      }
+      node = node.children[char];
+    }
+    return node;
+  }
+}
